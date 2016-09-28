39b9c0406a59bc3492bbf42c2f88ef61e1baf060
Victor Pongnian <victor.pongnian@gmail.com>
2016-09-28 21:06:01 +0100

    add chatwindow simple component

---
+++ app/client/app/components/ChatWindow.jsx
@@ -0,0 +1,16 @@
+/* eslint no-unused-vars: "off" */
+import React, { Component, PropTypes } from 'react'
+
+export default class listConnectedPeers extends Component {
+
+    constructor (props) {
+        super(props)
+        // this.state = { peersList: [], currentChat: {} }
+    }
+
+    render () {
+        return (
+            <div>Window here</div>
+        )
+    }
+}
