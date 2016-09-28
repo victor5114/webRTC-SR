f2ff2120ab5b6d7616f0fbf20fe91f507445ebe7
Victor Pongnian <victor.pongnian@gmail.com>
2016-09-28 21:06:27 +0100

    types datastring

---
+++ app/client/app/types/dataActions.js
@@ -0,0 +1,2 @@
+export const CHECK_AVAILABLE_PSEUDO = 'checkAvailablePseudo'
+export const AVAILABLE_PEERS = 'availablePeers'
---
+++ app/client/app/types/events.js
@@ -0,0 +1,2 @@
+export const ON_AVAILABLE_PSEUDO_RESPONSE = 'onavailablepseudoresponse'
+export const ON_AVAILABLE_PEERS_RESPONSE = 'onavailablepeersresponse'
